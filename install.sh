f42515a8730c647311286f6942361f206a5e8001
o <o>
2021-08-22 18:40:53 +0200

    correction

--- install.sh
+++ install.sh
@@ -6,7 +6,7 @@ then
 	make -C autocomplete
 	make -C autocomplete clean
 	cp autocomplete/autocomplete bin/.
-	printf "\nsource $PWD/zsh_conf.sh\n" >> ~.zshrc
+	printf "\nsource $PWD/zsh_conf.sh\n" >> ~/.zshrc
 	echo "installation complete. please restart your shell"
 else
 	echo "please add $PWD/bin to your PATH"
