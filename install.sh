0e0ada36d9f5ae967ab5123ad25b0a0f8e2326a1
o <o>
2021-08-22 19:00:29 +0200

    correction

--- install.sh
+++ install.sh
@@ -1,12 +1,15 @@
 #!/bin/zsh
 
-
 if [ -e ~/.zshrc ]
 then
 	make -C autocomplete
 	make -C autocomplete clean
 	cp autocomplete/api_autocomplete bin/.
-	printf "\nsource $PWD/zsh_conf.sh\n" >> ~/.zshrc
+	grep "source $PWD/zsh_conf.sh" ~/.zshrc > /dev/null
+	if [ $? -eq 1 ]
+	then
+		printf "\nsource $PWD/zsh_conf.sh\n" >> ~/.zshrc
+	fi
 	echo "installation complete. please specify the UID and SECRET of your application in zsh_conf.sh and restart your shell"
 else
 	echo "please add $PWD/bin to your PATH"
