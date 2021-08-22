f9435a26f833145c5c5a22621ff3faa9cdbd989d
o <o>
2021-08-22 18:51:44 +0200

    correction

--- zsh_conf.sh
+++ zsh_conf.sh
@@ -2,7 +2,7 @@ fpath=($PWD/completions $fpath)
 autoload -U compinit
 compinit
 zstyle ':completion:*' menu select=2
-PATH=$PATH:$PWD/bin
+export PATH=$PATH:$PWD/bin
 
-UID42=""
-SECRET42=""
+export UID42=""
+export SECRET42=""
